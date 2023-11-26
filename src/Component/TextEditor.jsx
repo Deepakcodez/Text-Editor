aa7d482fb5d5022f83fc56f4c77c67663ca78410
Deepak <deepak2603dk@gmail.com>
2023-11-26 13:43:12 +0530

    adding copy to clipboard fun and notify

--- src/Component/TextEditor.jsx
+++ src/Component/TextEditor.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
-import '../index.css'
+import "../index.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function TextEditor() {
+
   let [text, setText] = useState("");
   let [char, setChar] = useState(0);
   let [word, setWord] = useState(0);
@@ -10,7 +14,7 @@ export default function TextEditor() {
     let lenOfText = text.length - word;
     setChar(lenOfText);
     setWord(wordlen);
-  }, [text]);
+  }, [text,word]);
 
   const textHandler = (e) => {
     setText(e.target.value);
@@ -25,6 +29,35 @@ export default function TextEditor() {
     let newText = text.toLowerCase();
     setText(newText);
   };
+
+  const clearHandler = () =>{
+      setText('')
+      setChar(0)
+  }
+
+  const notify = () => toast("copy to clipboard!", {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    });
+
+  const copyHandler=()=>{
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        console.log('Text copied to clipboard');
+      })
+      .catch(err => {
+        console.error('Unable to copy text to clipboard', err);
+      });
+  }
+
+
+  
   return (
     <>
       <div className="container">
@@ -32,7 +65,7 @@ export default function TextEditor() {
           <textarea
             className="w-100 mt-4 mx-auto  p-2 bg-dark  "
             value={text}
-            style={{color:'#E0B0FF'}}
+            style={{ color: "#E0B0FF" }}
             onChange={textHandler}
           />
         </div>
@@ -52,6 +85,13 @@ export default function TextEditor() {
               {" "}
               to Lowercase
             </button>
+
+            <button className="btn btn-success mt-3 mx-1  " onClick={clearHandler}>
+              Clear{" "}
+            </button>
+            <button className="btn btn-success mt-3     " onClick={()=>{copyHandler(),notify()}}>
+              Copy
+            </button>
           </div>
         </div>
         <div className="row">
@@ -60,6 +100,8 @@ export default function TextEditor() {
           </div>
         </div>
       </div>
+      <ToastContainer />
+
     </>
   );
 }
